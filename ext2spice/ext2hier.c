d464a0475dec2befd9cbb7698708db4387402692
Darryl L. Miles <darryl.miles@darrylmiles.org>
2025-07-20 16:29:20 +0100

    extflat: integrate cb_extflat_hiervisitresists_t for EFHierVisitResists()
    Additionally the HierName's hierName1 and hierName2 arguments have been
    made 'const' to help convey the receiver can not modify the referenced
    data passed.

--- ext2spice/ext2hier.c
+++ ext2spice/ext2hier.c
@@ -1324,12 +1324,16 @@ spccapHierVisit(
  *
  * ----------------------------------------------------------------------------
  */
+
+/*ARGUSED*/
+/* @typedef cb_extflat_hiervisitresists_t (UNUSED) */
 int
 spcresistHierVisit(
     HierContext *hc,
-    HierName *hierName1,
-    HierName *hierName2,
-    float res)
+    const HierName *hierName1,
+    const HierName *hierName2,
+    float res,
+    ClientData cdata)	// UNUSED
 {
     HashEntry *he;
     EFNodeName *nn;
@@ -2217,7 +2221,7 @@ esHierVisit(
 	EFHierVisitDevs(hcf, spcdevHierVisit, PTR2CD(NULL));
 
 	/* Output lumped parasitic resistors */
-	EFHierVisitResists(hcf, spcresistHierVisit, (ClientData)NULL);
+	EFHierVisitResists(hcf, spcresistHierVisit, PTR2CD(NULL));
 
 	/* Output coupling capacitances */
 	EFHierVisitCaps(hcf, spccapHierVisit, PTR2CD(NULL));
--- extflat/EFhier.c
+++ extflat/EFhier.c
@@ -528,7 +528,7 @@ efHierVisitSingleResist(
     Connection *res,		/* Contains resistance to add */
     ClientData cdata)
 {
-    CallArg *ca = (CallArg *)CD2PTR(cdata);
+    CallArg *ca = (CallArg *) CD2PTR(cdata);
     EFNode *n1, *n2;
     HashEntry *he;
     Def *def = hc->hc_use->use_def;
@@ -562,13 +562,15 @@ efHierVisitSingleResist(
  * Visit all the resistors in the circuit.
  * For each resistor in the circuit, call the user-supplied procedure
  * (*resProc)(), which should be of the following form, where hn1 and
- * hn2 are the HierNames of the two nodes connected by the resistor.
+ * hn2 are the HierNames of the two nodes connected by the resistor
+ *   see also typedef cb_extflat_hiervisitresists_t:
  *
- *	(*resProc)(hc, hn1, hn2, resistance, cdata)
- *	    HierContext *hc;
- *	    HierName *hn1, *hn2;
- *	    int resistance;
- *	    ClientData cdata;
+ *	int (*resProc)(
+ *	    HierContext *hc,
+ *	    const HierName *hierName1,
+ *	    const HierName *hierName2,
+ *	    float resistance,
+ *	    ClientData cdata)
  *	{
  *	}
  *
@@ -591,13 +593,13 @@ efHierVisitSingleResist(
 int
 EFHierVisitResists(
     HierContext *hc,
-    int (*resProc)(),
+    const cb_extflat_hiervisitresists_t resProc,
     ClientData cdata)
 {
     CallArg ca;
     int efHierVisitResists();	/* Forward reference */
 
-    ca.ca_proc = resProc;
+    ca.ca_proc = (int (*)()) resProc;
     ca.ca_cdata = cdata;
     return efHierVisitResists(hc, (ClientData) &ca);
 }
@@ -633,7 +635,7 @@ efHierVisitResists(
 			res, ca))
 		return 1;
 	}
-	else if (efHierSrArray(hc, res, efHierVisitSingleResist, (ClientData) ca))
+	else if (efHierSrArray(hc, res, efHierVisitSingleResist, PTR2CD(ca)))
 	    return 1;
     }
     return 0;
